965850949230ede96cca136d8c8f808967c18587
Felicia Khan <feliciaiveliz@gmail.com>
2018-10-15 15:58:23 +0000

    Two new files for chapter 2.

---
+++ 2_Numbers/arithmetic.rb
@@ -0,0 +1,18 @@
+# Very basic output to the screen. Writes whatever comes after 'puts'
+puts 1 + 2
+
+# Basic mathematical operations
+puts 1.0 + 2.0
+puts 2.0 * 3.0 
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# Same operations with integers. Difference here with 'integer division; when it doesn't know right answer, it will round down and use integer answers.
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# More complex expressions
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
---
+++ 2_Numbers/time.rb
@@ -0,0 +1,20 @@
+# How many hours in a year?
+seconds = 60 # in an minute
+minutes = 60 # in an hour
+hours = 24 # in a day
+day = 365 # in a year
+puts seconds_in_a_year = 60*60*24*365
+puts seconds_in_leap_year = 60*60*24*366 # for kicks
+
+
+# How many minutes are in a decade?
+minutes = 60 # in an hour
+hours = 24 # in a day
+days = 365 # in a year
+years = 10 # in a decade
+puts 60*24*365*10 # ignoring leap years otherwise it would be 365.25
+
+
+# How many seconds old am I?
+years = 25
+puts 60*60*24*365*25
